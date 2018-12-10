dd8adeb1209a6b0768b684a63bafa0aefd2c897d
bfleischer-alti <bfleischer@altimetrik.com>
2018-12-10 14:42:52 -0300

    Added all fthat was done for US10-11-12

--- src/aura/Work_Item_Card/Work_Item_CardController.js
+++ src/aura/Work_Item_Card/Work_Item_CardController.js
@@ -1,4 +1,11 @@
 ({
-	myAction : function(component, event, helper) {
+	editWorkItem : function(component, event, helper) {
+		var target = event.currentTarget;
+		var id = target.getAttribute("data-id");
+        var editRecordEvent = $A.get("e.force:editRecord");
+        editRecordEvent.setParams({
+            "recordId": id
+        });
+        editRecordEvent.fire();
 	}
 })
--- src/aura/Work_Items_List/Work_Items_ListController.js
+++ src/aura/Work_Items_List/Work_Items_ListController.js
@@ -1,4 +1,20 @@
 ({
-	myAction : function(component, event, helper) {
+	doInit : function(component, event, helper) {
+		var statusType = component.get('v.workItemsStatus');
+
+		var action = component.get('c.getWorkItemsByStatusInLast30Days');
+		action.setParam('status', statusType);
+
+		action.setCallback(this, function(response){
+			var state = response.getState();
+			if(state == 'SUCCESS'){
+				var items = response.getReturnValue();
+				component.set('v.workItemsList', items);
+			}else{
+				// no else yet
+				console.log('Failed')
+			}
+		});
+		$A.enqueueAction(action);
 	}
 })
