e6ece536cdadc6037e336f107f393132b7d209a9
bfleischer-alti <bfleischer@altimetrik.com>
2018-12-13 10:20:46 -0300

    Part -3

--- src/aura/Work_Items_List/Work_Items_ListController.js
+++ src/aura/Work_Items_List/Work_Items_ListController.js
@@ -1,8 +1,16 @@
 ({
 	doInit : function(component, event, helper) {
 		var statusType = component.get('v.workItemsStatus');
-
-		var action = component.get('c.getWorkItemsByStatusInLast30Days');
+		var contact = component.get('v.contactRecordId');
+		
+		var action;
+		console.log("holaaa " + contact);
+		if(contact !== undefined && contact.length >= 0){
+			action = component.get('c.getWorkItemsByContactByStatusInLast30Days');
+			action.setParam('contact', contact);
+		}else{
+			action = component.get('c.getWorkItemsByStatusInLast30Days');
+		}
 		action.setParam('status', statusType);
 
 		action.setCallback(this, function(response){
